de0d08ed8533bda4665b9fc4f914e0ed3b066cef
Prashant G <prashant.gaikwad@leftrightmind.com>
2018-11-06 12:37:41 +0530

    stack view with Multiple views on one vc

---
+++ AutoLayout/TVViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TVViewController.swift
+//  AutoLayout
+//
+//  Created by Prashant G on 11/5/18.
+//  Copyright © 2018 MyOrg. All rights reserved.
+//
+
+import UIKit
+
+class TVViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+extension TVViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+}
+
+
